9ddc0bff82db6869952e5b8f9fbc6d1dc94bc2ec
Benjamin Arnold <B.Arnold@tekaris.com>
2016-02-23 09:13:41 +0100

    set inline arguments for navigation and clean controller

--- components/navigation/navigation.controller.js
+++ components/navigation/navigation.controller.js
@@ -5,12 +5,6 @@
         .module('fsApp.navigation', [])
         .controller('NavController', [NavController]);
     
-    function NavController() {
-        $('.button-collapse').sideNav({
-          menuWidth: 300, // Default is 240
-          edge: 'left', // Choose the horizontal origin
-          closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        });
-    };
+    function NavController() {}
     
 })();

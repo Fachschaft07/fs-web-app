194108fce168113353cdc7944426b53456e792da
Benjamin Arnold <barnold@hm.edu>
2016-03-22 08:20:52 +0100

    change timetable to tabs view

--- components/timetable/timetable.controller.js
+++ components/timetable/timetable.controller.js
@@ -6,7 +6,13 @@
 
   function TimetableController(dataFactory) {
     var timetable = this;
+
     timetable.items = [];
+    timetable.items.mon = [];
+    timetable.items.tue = [];
+    timetable.items.wed = [];
+    timetable.items.thurs = [];
+    timetable.items.fri = [];
 
     init();
 
@@ -29,12 +35,16 @@
           var pk = item.praktikum ? item.praktikum : null;
           dataFactory.getLesson({group: group, module: module, teacher: teacher, pk: pk})
               .then(function (result) {
+                timetable.countItems = result.data.length;
+
                 for (var i = 0; i < result.data.length; i++) {
                   var date = new Date();
                   date.setHours(result.data[i].hour);
                   date.setMinutes(result.data[i].minute);
                   result.data[i].date = date;
-                  timetable.items.push(result.data[i]);
+
+                  // Set timetable array for each day
+                  divideDataIntoDays(result.data[i]);
                 }
               });
         });
@@ -42,21 +52,25 @@
 
     } // end loadTimetableItems
 
-    // Filter to display the
-    timetable.mon = function (item) {
-      return (item.day === "MONDAY");
-    }
-    timetable.tue = function (item) {
-      return (item.day === "TUESDAY");
-    }
-    timetable.wed = function (item) {
-      return (item.day === "WEDNESDAY");
-    }
-    timetable.thurs = function (item) {
-      return (item.day === "THURSDAY");
-    }
-    timetable.fri = function (item) {
-      return (item.day === "FRIDAY");
+    function divideDataIntoDays(data) {
+      if (data.day === 'MONDAY') {
+        timetable.items.mon.push(data);
+      }
+      else if (data.day === 'TUESDAY') {
+        timetable.items.tue.push(data);
+      }
+      else if (data.day === 'WEDNESDAY') {
+        timetable.items.wed.push(data);
+      }
+      else if (data.day === 'THURSDAY') {
+        timetable.items.thurs.push(data);
+      }
+      else if (data.day === 'FRIDAY') {
+        timetable.items.fri.push(data);
+      }
+
+      //Disable Tab if array is empty
+      //Set active Tab on current day
     }
   }
 })();
